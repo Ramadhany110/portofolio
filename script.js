26ccce91875092663b2265e3c628f2d7cef5f19f
Ramadhany110 <119953242+Ramadhany110@users.noreply.github.com>
2022-12-16 01:05:47 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const textElement = ['Freelancer', 'Web Developer', 'Grapich designer'];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words = '';
+
+(function ngetik(){
+    if(count == textElement){
+        count = 0;
+    }
+    currentTxt = textElement [count];
+
+    words = currentTxt.slice(0, ++txtIndex);
+    document.querySelector('.efek-ngetik').textContent = words;
+
+    if(words.length == currentTxt.length){
+        count++;
+        txtIndex = 0;
+    }
+
+    setTimeout(ngetik, 500);
+    
+
+})();
